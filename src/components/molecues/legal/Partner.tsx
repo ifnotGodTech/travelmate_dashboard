75cc40d34f3ad6797c50f293afa680938f168ebd
devchrisedwin <officialedwinchris@gmail.com>
2025-04-08 13:45:06 +0100

    added edit functionality

--- src/components/molecues/legal/Partner.tsx
+++ src/components/molecues/legal/Partner.tsx
@@ -1,27 +1,52 @@
+
 import React from "react";
 
-type Props = {};
+type Props = {
+  isEditing: boolean;
+  content: string;
+  onContentChange: (content: string) => void;
+};
 
-const Partner = (props: Props) => {
+const Partner = ({ isEditing, content, onContentChange }: Props) => {
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+      onContentChange(e.target.value);
+    };
+  
   return (
     <div className="space-y-[40px]">
-      <Partners />
-      <Partners />
-      <Partners />
+
+    {isEditing ? (
+        <textarea
+          value={content}
+          onChange={handleChange}
+          className="w-full p-4 text-[16px] lg:text-[18px] font-[400] text-[#181818] leading-[30px] border border-gray-300 rounded-md"
+          rows={8}
+        />
+      ) : (
+        <>
+          <Partners content={content}/>
+          <Partners content={content}/>
+          <Partners content={content}/>
+        </>
+        
+      )}
     </div>
   );
 };
 
-const Partners = () => {
+type PartnersProps = {
+  content: string;
+};
+
+
+const Partners = ({ content }: PartnersProps) => {
   return (
     <div className="space-y-[20px]">
       <h1 className="text-[16px] lg:text-[20px] font-[400] text-[#181818] leading-[100%] ">
         Stay Partners
       </h1>
       <p className="text-[#4E4F52] text-[14px] lg:text-[20px] font-[400] leading-[100%] ">
-        From luxury five-star hotels to cozy boutique stays and budget-friendly
-        options, our accommodation partners offer variety and quality in every
-        corner of the globe.
+       {content}
       </p>
       <ul className="flex space-x-10 flex-wrap">
         <li className="text-[#4E4F52] text-[14px] lg:text-[20px] font-[400] leading-[100%]">
