4628edfb75ab029b71b9f68292541abe536c3149
Awe Oluwatimileyin Matthew <105941839+CodeATM@users.noreply.github.com>
2025-06-03 09:37:55 +0100

    support clean-up

--- src/services/roles/index.ts
+++ src/services/roles/index.ts
@@ -3,7 +3,7 @@ import env from "@/config/env";
 
 class Service {
   getRoles() {
-    return axios.get(env.api.superadmin + "roles/");
+    return axios.get(env.api.superadmin + "roles/admin-list/");
   }
   getMyRole() {
     return axios.get(env.api.roles + "roles/");
