eee94f4404bd228beb28afb5e9b41dc1c761133b
Awe Oluwatimileyin Matthew <105941839+CodeATM@users.noreply.github.com>
2025-06-12 10:19:22 +0100

    changes

--- src/components/molecues/support/Chats/ChatReuseables.tsx
+++ src/components/molecues/support/Chats/ChatReuseables.tsx
@@ -225,8 +225,7 @@ export const ClaimedChatSection = ({
   const { onClaiming, claiming } = useClaimChat();
   const currentUser = APP_STATE?.user?.user_id || "";
   const { loading, data } = useMyRoles({ modalVisible: !!chatDetails });
-  const canViewMessage =
-    data?.name === "Support & Tickets" || data?.name === "Super Admin";
+  const canViewMessage = data?.name === "Support & Tickets";
 
   const [notAuthorized, setNotAuthorized] = useState(false);
   const [isProcessing, setIsProcessing] = useState(false);
